58ddf7e500b8c2c3d93008db8ae04b7acf6c731e
myfour <oz_myx@126.com>
2018-03-19 12:45:10 +0800

    about @Mapper Interface

--- SpringBoot_Mybatis_Mapper/src/main/java/com/myfour/dao/LearnResourceMapper.java
+++ SpringBoot_Mybatis_Mapper/src/main/java/com/myfour/dao/LearnResourceMapper.java
@@ -2,9 +2,11 @@ package com.myfour.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.myfour.domain.LearnResource;
 import com.myfour.util.MyMapper;
-
+//@Mapper
 public interface LearnResourceMapper extends MyMapper<LearnResource> {
 	List<LearnResource> queryLearnResourceList(LearnResource learnResource);
 }
